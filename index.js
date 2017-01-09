9fca29ce284ffe153dc93c700c56c56531a2bfd1
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-09 02:53:23 +0000

    Add panic mode responses for retrieving bubbles

--- index.js
+++ index.js
@@ -62,11 +62,64 @@ app.get('/friends/nearby', auth, function(req, res, next) {
 })
 
 app.post('/bubbles', auth, function(req, res, next) {
+  log.info({text: req.body.text, exclude_ids: req.body.exclude_ids});
   if( panicMode ) { return res.status(201).json({id: 'PANICMODE'}); }
 })
 
 app.delete('/bubbles/:id', auth, function(req, res, next) {
   if( panicMode ) { return res.sendStatus(204); }
+})
+
+app.get('/bubbles/mine', auth, function(req, res, next) {
+  if( panicMode ) {
+    return res.json({
+      title: "hmm, mewbe I should work for this app",
+      created_at: +new Date - 1000 * 60 * 60,
+      user: {
+        avatar_url: 'https://placekitten.com/640/640',
+        name: 'You',
+      },
+      attendees: [
+        {
+          avatar_url: 'https://placekitten.com/640/640',
+          name: "You kitten me?",
+          joined_at: +new Date,
+        },
+        {
+          avatar_url: 'https://placekitten.com/640/640',
+          name: "This is a catastrophe.",
+          joined_at: +new Date,
+        },
+      ]
+    })
+  }
+})
+
+app.get('/bubbles', auth, function(req, res, next) {
+  if( panicMode ) {
+    return res.json({
+      bubbles: [
+        {
+          title: 'Is everything down?',
+          created_at: +new Date - 1000 * 60 * 35,
+          user: {
+            name: 'Yep',
+            avatar_url: 'https://placekitten.com/640/640',
+          },
+          attending: false,
+        },
+        {
+          title: 'Still?',
+          created_at: +new Date - 1000 * 60 * 120,
+          user: {
+            name: 'Wow',
+            avatar_url: 'https://placekitten.com/640/640',
+          },
+          attending: true,
+        },
+      ]
+    })
+  }
 })
 
 app.use(function(err, req, res, next) {
@@ -83,8 +136,6 @@ function auth(req, res, next) {
   if( panicMode ) { return next(); }
 
   const token = req.get('X-Access-Token');
-
-  console.log("checking token", token, Object.keys(sessions));
 
   if( !token || !sessions[token] ) {
     return res.status(401).json({error: "Invalid Access Token", token: token});
