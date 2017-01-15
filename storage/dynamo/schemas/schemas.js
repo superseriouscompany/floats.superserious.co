6724407b1fafce22aa6bbdd11ca63eca740037e6
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-15 21:11:48 +0000

    Add script to drop tables

--- storage/dynamo/schemas/schemas.js
+++ storage/dynamo/schemas/schemas.js
@@ -1,3 +1,31 @@
+const config = require('../../../config');
+
 module.exports = {
-  pins: {}
+  pins: {
+    "TableName": config.pinsTableName,
+    "AttributeDefinitions":[
+      {
+        "AttributeName":"user_id",
+        "AttributeType":"S"
+      },
+      {
+        "AttributeName":"created_at",
+        "AttributeType":"N"
+      },
+    ],
+    "KeySchema":[
+      {
+        "AttributeName":"user_id",
+        "KeyType":"HASH"
+      },
+      {
+        "AttributeName":"created_at",
+        "KeyType":"RANGE"
+      },
+    ],
+    "ProvisionedThroughput": {
+      "ReadCapacityUnits":1,
+      "WriteCapacityUnits":5
+    },
+  },
 }
