197810bbb4de83be00a9f9e93d372ad1cefbc728
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-15 15:39:09 +0000

    Do approvals with PUT

--- controllers/friend_requests.js
+++ controllers/friend_requests.js
@@ -4,9 +4,8 @@ const auth  = require('../services/auth');
 const panic = require('../services/panic');
 
 module.exports = function(app) {
-  app.post('/randos/:id/friend_request', auth, create);
-  app.post('/friend_requests/:id/approve', auth, approve);
-  app.post('/friend_requests/:id/undo', auth, undo);
+  app.post('/friend_requests', auth, create);
+  app.put('/friend_requests/:id', auth, approve);
   app.delete('/friend_requests/:id', auth, reject);
   app.get('/friend_requests', auth, all);
 }
@@ -34,7 +33,7 @@ function reject(req, res, next) {
 }
 
 function all(req, res, next) {
-  if( process.env.PANIC_MODE ) { return res.json(panic.friendRequests); }
+  if( process.env.PANIC_MODE ) { return res.json({friend_requests: panic.friendRequests}); }
 
   next('not implemented');
 }
