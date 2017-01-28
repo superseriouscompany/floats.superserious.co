8ecd9ebcf5de8c80379cbbf10fa475d1bd22ee5a
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-01-28 16:21:59 +0000

    Move messages tests to api folder

--- test/api/floats.js
+++ test/api/floats.js
@@ -17,8 +17,6 @@ module.exports = function() { describe("/floats", function() {
     user  = null;
     convo = null;
     float = null;
-    if( process.env.LIVE ) { return; }
-
     return h.clearStub();
   })
 
